57aea9f6e2263de9df7a4f2c1f697f3f925f4be4
Yuleo <1173012900@qq.com>
2025-02-09 21:28:35 +0800

    css重置

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import './assets/main.css';
-
 import { createApp } from 'vue';
 import App from './App.vue';
+import 'normalize.css';  //css重置样式
+import './assets/css/index.less'; //npm install less -D
 
 createApp(App).mount('#app');
