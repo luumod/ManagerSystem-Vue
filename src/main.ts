a6ba050c821608086c3bb96567a2f60ecf5cc299
Yuleo <1173012900@qq.com>
2025-02-10 15:32:48 +0800

    ElementPlus

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import 'normalize.css'; //css重置样式
 import './assets/css/index.less'; //npm install less -D
+// import ElementPlus from 'element-plus'; //全局注册
+// import 'element-plus/dist/index.css';
+// app.use(ElementPlus);
 import router from './router';
 import pinia from './store';
 
 const app = createApp(App);
 app.use(router);
-app.use(pinia)
+app.use(pinia);
 app.mount('#app');
