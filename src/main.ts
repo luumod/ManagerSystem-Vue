85bbd76cbb9227f83b54fc96c7d2235f73ecb666
Yuleo <1173012900@qq.com>
2025-02-10 10:55:14 +0800

    pinia配置

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import App from './App.vue';
 import 'normalize.css'; //css重置样式
 import './assets/css/index.less'; //npm install less -D
 import router from './router';
+import pinia from './store';
 
 const app = createApp(App);
 app.use(router);
+app.use(pinia)
 app.mount('#app');
--- src/views/main/Main.vue
+++ src/views/main/Main.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="main">
-    <h2>main</h2>
+    <h2>main:{{ counterStore.counter }} - {{ counterStore.doubleCounter }}</h2>
+    <button @click="changeCounter">修改counter</button>
   </div>
 </template>
 
-<script setup lang="ts" name="main"></script>
+<script setup lang="ts" name="main">
+import useCounterStore from '@/store/counter';
+
+const counterStore = useCounterStore();
+
+function changeCounter() {
+  counterStore.changeCounterAction(200);
+}
+</script>
 
 <style scoped>
 .main {
