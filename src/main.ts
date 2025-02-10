1c4e0ce11a2b44d0da831b42f23c06b2305e1988
Yuleo <1173012900@qq.com>
2025-02-10 16:01:33 +0800

    element-plus配置

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,18 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import 'normalize.css'; //css重置样式
 import './assets/css/index.less'; //npm install less -D
-// import ElementPlus from 'element-plus'; //全局注册
+// 全局注册
+// import ElementPlus from 'element-plus';
 // import 'element-plus/dist/index.css';
 // app.use(ElementPlus);
 import router from './router';
 import pinia from './store';
 
+// 局部注册
+//import { ElButton } from 'element-plus';
+//app.component(ElButton.name, ElButton);
+
+// 自动注册
 const app = createApp(App);
 app.use(router);
 app.use(pinia);
