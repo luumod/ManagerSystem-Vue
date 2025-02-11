18c1bfd3eb14fb9a2ce4de3ce15443c43160082e
Yuleo <1173012900@qq.com>
2025-02-11 20:10:35 +0800

    手机登录

--- src/views/login/c-cpns/login-phone.vue
+++ src/views/login/c-cpns/login-phone.vue
@@ -1,13 +1,32 @@
 <template>
   <div class="login-phone">
-    <h2>login-phone</h2>
+    <el-form label-width="60px">
+      <el-form-item label="手机号">
+        <el-input v-model="phone" placeholder="请输入手机号"/>
+      </el-form-item>
+      <el-form-item label="验证码">
+        <div class="verify-code">
+          <el-input/>
+          <el-button class="getCode-btn" type="primary">获取验证码</el-button>
+        </div>
+
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
-<script setup lang="ts" name="login-phone"></script>
+<script setup lang="ts" name="login-phone">
+import { ref } from 'vue'
 
-<style scoped>
-.login-phone{
-  color: red;
+const phone = ref('')
+</script>
+
+<style scoped lang="less">
+.verify-code{
+  display: flex;
+  .getCode-btn{
+  margin-left: 10px;
 }
+}
+
 </style>
