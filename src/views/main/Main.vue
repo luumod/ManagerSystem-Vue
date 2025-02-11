60927aea760f0590276d190d3bef1dce0bd09960
Yuleo <1173012900@qq.com>
2025-02-11 19:14:44 +0800

    退出登录

--- src/views/main/Main.vue
+++ src/views/main/Main.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="main">
-    <h2>main:{{ counterStore.counter }} - {{ counterStore.doubleCounter }}</h2>
-    <button @click="changeCounter">修改counter</button>
+    <button @click="exitLogin">退出登录</button>
   </div>
 </template>
 
 <script setup lang="ts" name="main">
-import useCounterStore from '@/store/counter';
+import { LOGIN_TOKEN } from '@/global/constants';
+import router from '@/router';
+import { localCache } from '@/utils/cache';
 
-const counterStore = useCounterStore();
-
-function changeCounter() {
-  counterStore.changeCounterAction(200);
+function exitLogin() {
+  //1. 清除token
+  localCache.removeCache(LOGIN_TOKEN);
+  //2. 跳转到登录页面
+  router.push('/login');
 }
 </script>
 
