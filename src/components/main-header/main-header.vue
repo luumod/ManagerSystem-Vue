75c7310f69ee33b713e93a4c407f930a40a175cb
Yuleo <1173012900@qq.com>
2025-02-12 14:19:40 +0800

    头部布局

--- src/components/main-header/main-header.vue
+++ src/components/main-header/main-header.vue
@@ -1,13 +1,36 @@
 <template>
   <div class="main-header">
-    <h2>main-header</h2>
+    <div class="menu-icon">
+      <el-icon size="25"><Fold /></el-icon>
+    </div>
+    <div class="content">
+      <div class="breadcrumb">面包屑</div>
+      <div class="info">个人信息</div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts" name="main-header"></script>
 
-<style scoped>
-.main-header{
-  color: red;
+<style scoped lang="less">
+.main-header {
+  display: flex;
+  align-items: center;
+  flex: 1;
+  height: 100%;
+
+  .menu-icon {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+  }
+
+  .content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex: 1;
+    padding: 0 18px;
+  }
 }
 </style>
